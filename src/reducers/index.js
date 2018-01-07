99246602cc76930aa7f582a330579930b33ca5bb
raulif <raul4cade@gmail.com>
2018-01-07 21:17:52 +0100

    Added reducer for selected posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import PostsReducer from './reducer_posts';
+import posts from './reducer_posts';
+import selectedPostIds from './reducer_selected_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer,
+  posts,
+  selectedPostsIds,
   form: formReducer
 });
 
